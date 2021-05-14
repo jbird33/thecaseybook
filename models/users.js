4e634c24e6b8d4cf37bf1e67135090fc92a8cb62
William Jeremiah Miller <iith001@upcorp.ad.uprr.com>
2021-05-14 11:32:32 -0500

    Built out array of objects

---
+++ models/users.js
@@ -0,0 +1,10 @@
+const users = [
+    {
+        username: "jeremiahm33",
+        email: "wjmiller@URLSearchParams.com",
+        password: "christian22",
+        aboutMe: "This is where something is written about me",
+        img: "https://www.cnet.com/a/img/d9J657AsEFxuQiUGOQIMJKEUxAM=/2019/06/27/a6b10caf-cfd4-49c6-b664-db51c84cd9f5/watch-some-crazy-fat-thor-stunts-in-bts-videos-from-avengers-endgame-social.jpg",
+        uuid: 12345
+    }
+]
